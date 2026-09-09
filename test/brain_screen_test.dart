f2ac92bcbd5c7c6c6e05b92b8ae86b50d3dbf8f0
warishakhan3548-hash <warishakhan3548@gmail.com>
2026-09-10 02:57:05 +0530

    Fix operational context test import

--- test/brain_screen_test.dart
+++ test/brain_screen_test.dart
@@ -1,6 +1,7 @@
 import 'package:aaris_pharmacy/data/inventory_database.dart';
 import 'package:aaris_pharmacy/domain/app_brain.dart';
 import 'package:aaris_pharmacy/domain/medicine.dart';
+import 'package:aaris_pharmacy/state/operational_context.dart';
 import 'package:aaris_pharmacy/state/pharmacy_controller.dart';
 import 'package:aaris_pharmacy/ui/brain_screen.dart';
 import 'package:aaris_pharmacy/ui/design.dart';
