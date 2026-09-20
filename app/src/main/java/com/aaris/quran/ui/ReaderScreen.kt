2b58c06c7ea56864cb7a7e20490ca7e62201f157
warishakhan3548-hash <warishakhan3548@gmail.com>
2026-09-20 23:05:51 +0530

    Fix Compose scoped weight resolution

--- app/src/main/java/com/aaris/quran/ui/ReaderScreen.kt
+++ app/src/main/java/com/aaris/quran/ui/ReaderScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
