d6d9d3a882bbb62c5caa4c083fdc02b16ab322c9
warishakhan3548-hash <warishakhan3548@gmail.com>
2026-09-20 22:35:16 +0530

    Add Phase 1 reader foundation: app/src/main/java/com/aaris/quran/MainActivity.kt

---
+++ app/src/main/java/com/aaris/quran/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.aaris.quran
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import com.aaris.quran.data.QuranPackStore
+import com.aaris.quran.ui.QuranReaderScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val store = QuranPackStore(applicationContext)
+
+        setContent {
+            MaterialTheme(
+                colorScheme = if (isSystemInDarkTheme()) {
+                    darkColorScheme()
+                } else {
+                    lightColorScheme()
+                },
+            ) {
+                QuranReaderScreen(
+                    store = store,
+                    onOpenSource = {
+                        runCatching {
+                            startActivity(
+                                Intent(
+                                    Intent.ACTION_VIEW,
+                                    Uri.parse("https://tanzil.net/"),
+                                ),
+                            )
+                        }
+                    },
+                )
+            }
+        }
+    }
+}
