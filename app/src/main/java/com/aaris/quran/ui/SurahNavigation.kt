85cd9354bc6e7d4537299425f1f53d6774460b16
warishakhan3548-hash <warishakhan3548@gmail.com>
2026-09-20 22:51:54 +0530

    Add Android reader foundation: app/src/main/java/com/aaris/quran/ui/SurahNavigation.kt

---
+++ app/src/main/java/com/aaris/quran/ui/SurahNavigation.kt
@@ -0,0 +1,16 @@
+package com.aaris.quran.ui
+
+object SurahNavigation {
+    const val FIRST_SURAH = 1
+    const val LAST_SURAH = 114
+
+    fun previous(current: Int): Int {
+        require(current in FIRST_SURAH..LAST_SURAH)
+        return (current - 1).coerceAtLeast(FIRST_SURAH)
+    }
+
+    fun next(current: Int): Int {
+        require(current in FIRST_SURAH..LAST_SURAH)
+        return (current + 1).coerceAtMost(LAST_SURAH)
+    }
+}
