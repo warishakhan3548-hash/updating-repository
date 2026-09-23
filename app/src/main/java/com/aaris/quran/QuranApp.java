8e39c138b54bfb364fdc0407fa3b6eae7e791ebf
warishakhan3548-hash <warishakhan3548@gmail.com>
2026-09-23 08:12:37 +0530

    Load Hadith pack only when bundled and verified [skip ci]

--- app/src/main/java/com/aaris/quran/QuranApp.java
+++ app/src/main/java/com/aaris/quran/QuranApp.java
@@ -38,7 +38,7 @@ public final class QuranApp extends Application {
             public void onActivityDestroyed(Activity a){}
         });
         exports=new ExportStaging(new File(getFilesDir(),"export-staging"));io.execute(()->{
-            try{content=new ContentStore(this);learning=new LearningStore(this);learning.getWritableDatabase();hadith=new HadithStore(this);}
+            try{content=new ContentStore(this);learning=new LearningStore(this);learning.getWritableDatabase();hadith=HadithStore.openIfBundled(this);}
             catch(Exception e){loadError="Offline content khul nahi saka: "+e.getMessage();}
             finally{ready.countDown();}
         });
